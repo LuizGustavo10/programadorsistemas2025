f40f7f06131e8a0314ac6fe06a26ac9c1da8d182
LuizGustavo10 <gustavo180g@gmail.com>
2025-06-23 17:29:31 -0300

    jogo da velha

---
+++ jogo da velha/script.js
@@ -0,0 +1,20 @@
+var jogador = "x";
+
+function jogar(celula){
+    
+    //compara se a célula está vazia para assim escrever
+    if(celula.innerHTML == ""){
+
+        //escreva a letra na célula
+        celula.innerHTML = jogador;
+
+        //alterna a variável jogador para a próxima jogada
+        if(jogador == "x"){
+            jogador = "o";
+            celula.style.backgroundColor = "red";
+        }else{
+            jogador = "x";
+            celula.style.backgroundColor = "blue";
+        }
+    }
+}
